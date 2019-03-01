3c221e84ddf02d5a8c79c12402e0f817dc0beded
Navjot Kaur <navjotkaur629@gmail.com>
2019-03-01 13:01:06 +0530

    [01-03-2019] Added examples for debounce, throttling and timer

---
+++ src/timer.js
@@ -0,0 +1,11 @@
+// SETTIMEOUT
+const setTimeoutId = setTimeout(() => console.log('Set timeout example'), 500);
+
+// SETINTERVAL
+const setIntervalId = setInterval(() => console.log('Set interval example'), 500);
+
+// CLEAR TIMEOUT
+clearTimeout(setTimeoutId);
+
+// CLEAR INTERVAL
+clearInterval(setIntervalId);
